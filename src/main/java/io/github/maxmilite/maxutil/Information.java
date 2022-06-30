c4c4a52227ff2e143f8ee264d40f197b82d4387d
Maxmilite <yufe008@live.com>
2022-06-30 23:20:25 +0800

    Update v1.0.2

--- src/main/java/io/github/maxmilite/maxutil/Information.java
+++ src/main/java/io/github/maxmilite/maxutil/Information.java
@@ -3,6 +3,6 @@ package io.github.maxmilite.maxutil;
 public class Information {
     public static String clientName = "MaxUtil Client";
     public static String name = "MaxUtil";
-    public static String version = "1.0.1";
+    public static String version = "1.0.2";
     public static String mcVersion = "1.19";
 }
